183ef362dcf0427ba15f937d5eafe85006fc5314
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2017-10-31 21:37:43 +0100

    Connect classifier to reddit provider

--- infosecbot/classifier.py
+++ infosecbot/classifier.py
@@ -2,7 +2,7 @@
 from textblob.classifiers import NaiveBayesClassifier
 import pickle
 import sys
-
+import provider.reddit as reddit
 
 def learn_basic():
     train = []
@@ -15,12 +15,36 @@ def learn_basic():
             train.append((line, False))
 
     cl = NaiveBayesClassifier(train)
+
+
+def learn_reddit():
+    train = []
+    spam = reddit.gather_urls(["worldnews", "programmerhumor"], "top")
+    for s in spam:
+        train.append((s['title'], False))
+
+    spam = reddit.gather_urls(None, "top")
+    for s in spam:
+        train.append((s['title'], True))
+
+    return train
+
+
+def load_classifier():
+    with open('classifier.pickle', 'rb') as fp:
+        return pickle.load(fp)
+
+
+def save_classifier(cl):
     with open('classifier.pickle', 'wb') as fp:
         pickle.dump(cl, fp) 
 
 
 if __name__ == "__main__":
     # learn_basic()
-    with open('classifier.pickle', 'rb') as fp:
-        cl = pickle.load(fp)
-    print(cl.classify(sys.argv[1]))
+    cl = load_classifier()
+    cl.update(learn_reddit())
+    save_classifier(cl)
+
+    if False:
+        print(cl.classify(sys.argv[1]))
--- infosecbot/provider/reddit.py
+++ infosecbot/provider/reddit.py
@@ -1,18 +1,23 @@
 from webclient import webclient
 from storage import storage
 
-reddit_url = "https://www.reddit.com/r/crypto+netsec+hacking/new/.json"
+default_reddits = ['crypto', 'netsec', 'hacking']
+default_sort = 'new'
 
 
-def get_links(id):
+def get_links(id, reddits, sort):
+    reddit_url = "https://www.reddit.com/r/%s/%s/.json" % ("+".join(reddits), sort)
     response = webclient.get(reddit_url)
     response.raise_for_status()
     return response.json()
 
 
-def gather_urls():
+def gather_urls(reddits=None, sort=None):
+    reddits = reddits or default_reddits
+    sort = sort or default_sort
+
     last_id = storage["reddit"]["last_id"]
-    links = get_links(last_id)
+    links = get_links(last_id, reddits, sort)
     children = [c['data'] for c in links['data']['children']]
     return [c for c in children if not c['is_self']]
 
