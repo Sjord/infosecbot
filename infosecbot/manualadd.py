6c3d595b4ab3b8acb7f7ae548a6fd3d9aecdf59b
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2018-06-13 09:07:56 +0200

    Add source to manually added links, catch excptions

--- infosecbot/manualadd.py
+++ infosecbot/manualadd.py
@@ -1,15 +1,19 @@
 import sys
 from infosecbot.webpage import retrieve_title
 from infosecbot.storage import storage
-from infosecbot.model import Link
+from infosecbot.model import Link, Source
 
 if __name__ == "__main__":
     for url in sys.argv[1:]:
-        link = storage.find_link_by_url(url)
-        if link is None:
-            title = retrieve_title(url)
-            link = Link(url, title)
-            storage['links'].append(link)
-            print("Created new link for " + str(link))
-        link.score += 1
+        try:
+            link = storage.find_link_by_url(url)
+            if link is None:
+                title = retrieve_title(url)
+                source = Source("manual", None)
+                link = Link(url, title, source)
+                storage['links'].append(link)
+                print("Created new link for " + str(link))
+            link.score += 1
+        except Exception as e:
+            print("Failed to add '%s': %s" % (url, str(e)))
     storage.save()
