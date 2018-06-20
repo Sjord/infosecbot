e5812298e9876c7a0c7fdc1894920087432525c2
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2018-06-20 16:29:53 +0200

    Run only one instance of collector at a time

--- infosecbot/collector.py
+++ infosecbot/collector.py
@@ -3,6 +3,7 @@ import infosecbot.provider.hackernews as hackernews
 import infosecbot.provider.twitter as twitter
 from infosecbot.classifier import load_classifier
 from infosecbot.storage import storage
+from infosecbot.lockfile import LockFile
 import sys
 
 
@@ -56,19 +57,20 @@ def is_probably_infosec(link):
 
 
 if __name__ == "__main__":
-    new_links = []
+    with LockFile():
+        new_links = []
 
-    try:
-        for l in collect_links():
-            classify(l)
-            voted = autovote(l)
-            if voted or is_probably_infosec(l):
-                storage['links'].append(l)
-                if is_probably_infosec(l):
-                    new_links.append(l)
-    finally:
-        storage.save()
-        raise
+        try:
+            for l in collect_links():
+                classify(l)
+                voted = autovote(l)
+                if voted or is_probably_infosec(l):
+                    storage['links'].append(l)
+                    if is_probably_infosec(l):
+                        new_links.append(l)
+        finally:
+            storage.save()
+            raise
 
-    if "tweet" in sys.argv:
-        twitter.handle_new_links(new_links)
+        if "tweet" in sys.argv:
+            twitter.handle_new_links(new_links)
---
+++ infosecbot/lockfile.py
@@ -0,0 +1,11 @@
+from fcntl import flock, LOCK_EX, LOCK_NB
+
+class LockFile:
+    lockfile_name = "lockfile"
+
+    def __enter__(self):
+        self.fd = open(self.lockfile_name, "w")
+        flock(self.fd, LOCK_EX | LOCK_NB)
+
+    def __exit__(self, type, value, traceback):
+        self.fd.close()
