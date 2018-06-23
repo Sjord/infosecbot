2271d054c7d917591f7ea343861ea517be651341
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2018-06-23 22:15:53 +0200

    Wordlist based classifier start

--- infosecbot/classifier.py
+++ infosecbot/classifier.py
@@ -1,79 +1,69 @@
-
-from textblob.classifiers import NaiveBayesClassifier
-import pickle
-import sys
-import infosecbot.provider.reddit as reddit
+from nltk import NaiveBayesClassifier
+import nltk
 from infosecbot.storage import storage
-from infosecbot.linkclassifier import LinkClassifier
+import re
 
-def learn_basic():
-    train = []
-    with open('positive.txt') as fp:
+
+def link_features(link, words, prop):
+    return {w: w in link.__dict__[prop] for w in words}
+
+
+def load_words(wordfile):
+    words = []
+    with open(wordfile) as fp:
         for line in fp:
-            train.append((line, True))
-
-    with open('negative.txt') as fp:
-        for line in fp:
-            train.append((line, False))
-
-    cl = NaiveBayesClassifier(train)
-    return cl
+            words.append(line.strip())
+    return words
 
 
-def learn_reddit():
-    train = []
-    spam = reddit.gather_urls(["worldnews", "programmerhumor"], "top")
-    for s in spam:
-        train.append((s['title'], False))
-
-    spam = reddit.gather_urls(None, "top")
-    for s in spam:
-        train.append((s['title'], True))
-
-    return train
-
-
-def load_classifier():
-    with open('classifier.pickle', 'rb') as fp:
-        return pickle.load(fp)
-
-
-def save_classifier(cl):
-    with open('classifier.pickle', 'wb') as fp:
-        pickle.dump(cl, fp) 
-
-
-def get_learnable_links():
-    train = []
+def load_bayes():
+    words = load_words("titlewords.txt")
+    feature_sets = []
     for link in storage['links']:
-        if link.score != link.learned_at_score and link.score != 0:
-            train.append(link)
-    return train
+        if link.score != 0:
+            features = {w: re.search(r"\b"+w+r"\b", link.title.lower()) is not None for w in words}
+            feature_sets.append((features, link.score > 0))
+    train_set = feature_sets[0::2]
+    test_set = feature_sets[1::2]
+    bayes = NaiveBayesClassifier.train(train_set)
+    print(nltk.classify.accuracy(bayes, test_set))
+
+def load_bayes2():
+    words = load_words("domainwords.txt")
+    feature_sets = []
+    for link in storage['links']:
+        if link.score != 0:
+            features = {w: w in link.domain for w in words}
+            feature_sets.append((features, link.score > 0))
+    train_set = feature_sets[0::2]
+    test_set = feature_sets[1::2]
+    bayes = NaiveBayesClassifier.train(train_set)
+    print(nltk.classify.accuracy(bayes, test_set))
 
 
-def initialize_classifier():
-    bayes = learn_basic()
-    classifier = LinkClassifier(bayes)
-    classifier.learn(storage['links'])
-    return classifier
+def load_bayes3(titlewords):
+    # titlewords = load_words("titlewords.txt")
+    domainwords = load_words("domainwords.txt")
+    feature_sets = []
+    for link in storage['links']:
+        if link.score != 0:
+            features = {"domain-" + w: w in link.domain for w in domainwords}
+            features.update({"title-" + w: re.search(r"\b"+w+r"\b", link.title.lower()) is not None for w in titlewords})
+            features["is-https"] = link.url.startswith("https:")
+            feature_sets.append((features, link.score > 0))
+    train_set = feature_sets[0::2]
+    test_set = feature_sets[1::2]
+    bayes = NaiveBayesClassifier.train(train_set)
+    return nltk.classify.accuracy(bayes, test_set)
+
+
+
+
+    
 
 
 if __name__ == "__main__":
-    cl = load_classifier()
-
-    action = sys.argv[1]
-    if action == "show":
-        cl.bayes.show_informative_features(n=100)
-
-        for link in storage['links'][0:100]:
-            prob = cl.classify(link)
-            print(link, prob)
-    elif action == "init":
-        cl = initialize_classifier()
-        save_classifier(cl)
-    elif action == "update":
-        cl.learn(get_learnable_links())
-        save_classifier(cl)
-        storage.save()
-    else:
-        raise ValueError("action")
+    titlewords = load_words("titlewords.txt")
+    print(load_bayes3(titlewords))
+    for i in range(len(titlewords)):
+        print(load_bayes3(titlewords[0:i] + titlewords[i+1:]), titlewords[i])
