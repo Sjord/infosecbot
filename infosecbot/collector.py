9f059c44222ac96576b091359e003f3f5d0575e4
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2018-12-26 14:48:36 +0100

    Formatting

--- infosecbot/collector.py
+++ infosecbot/collector.py
@@ -1,7 +1,7 @@
 import infosecbot.provider.reddit as reddit
 import infosecbot.provider.hackernews as hackernews
 import infosecbot.provider.twitter as twitter
-from infosecbot.classifier import LinkClassifier 
+from infosecbot.classifier import LinkClassifier
 from infosecbot.storage import storage
 from infosecbot.lockfile import LockFile
 from random import randrange
@@ -10,8 +10,8 @@ import sys
 
 class SeenIt:
     def __init__(self):
-        self.seen_urls = set([l.url for l in storage['links']])
-        self.seen_titles = set([l.title for l in storage['links']])
+        self.seen_urls = set([l.url for l in storage["links"]])
+        self.seen_titles = set([l.title for l in storage["links"]])
 
     def seen(self, link):
         seen = link.url in self.seen_urls or link.title in self.seen_titles
@@ -28,12 +28,12 @@ def collect_links():
         links = p.gather_urls()
         for link in links:
             if not seenit.seen(link):
-                yield(link)
+                yield (link)
 
 
 def autovote(link):
     prob = link.infosec_probability
-    assert(prob)
+    assert prob
 
     if prob > 0.999:
         link.score += 1
@@ -60,7 +60,7 @@ if __name__ == "__main__":
                 l.infosec_probability = classifier.classify(l)
                 voted = autovote(l)
                 if voted or is_probably_infosec(l):
-                    storage['links'].append(l)
+                    storage["links"].append(l)
                     if is_probably_infosec(l):
                         new_links.append(l)
         finally:
